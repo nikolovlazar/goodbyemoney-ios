cf5786a85996498b5fa4c44fe42d098635919493
Lazar Nikolov <hello@nikolovlazar.com>
2022-09-15 17:17:49 +0200

    Initial Commit

---
+++ GoodbyeMoney/GoodbyeMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodbyeMoneyApp.swift
+//  GoodbyeMoney
+//
+//  Created by Lazar Nikolov on 2022-09-15.
+//
+
+import SwiftUI
+
+@main
+struct GoodbyeMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
